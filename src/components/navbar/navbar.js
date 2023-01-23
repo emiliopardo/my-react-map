a456c08a95369d25ed0bba4a35fcd5400b52c5fb
Emilio Pardo <e.pardo.perez@gmail.com>
2023-01-23 09:32:33 +0100

    se crea cabecera y se generan css

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './navbar.css';
+import logo_cvb100v from './img/logos/logo_cvb100v.jpg'
 
 export default function Navbar() {
     return (
-        <div className="heading">
-            <h1>This is my map App</h1>
+        <div className='heading'>
+            <div id='left'>
+            <div id='header_corpus'>CORPUS VRBIVM BAETICARVM</div>
+            <div id='header_conventvs'>CONVENTVS HISPALENSIS ET ASTIGITANVS - CORDUBENSIS Y GADITANUS EN CONSTRUCCIÓN -</div>
+            </div>
+            <div id='right'>
+                <a title='Corpus' href='http://cvb.vrbanitas.es/lista_ciudades' target='_blank'>
+                    <img src={logo_cvb100v} alt="logo" />
+                </a>
+            </div>
         </div>
     );
 }
