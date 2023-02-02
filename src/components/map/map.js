fa404c4798ca0c8c7cdb414cf97aa7fd71dc5521
Emilio Pardo <e.pardo.perez@gmail.com>
2023-02-02 12:54:21 +0100

    se corrigen vector tiles poligonales y se actualiza configuracion del mapa

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -49,191 +49,306 @@ export default function Map() {
         });
 
         map.current.on('style.load', function () {
-            map.current.addSource('corpus_urbium_baeticarum', {
+            map.current.addSource('test', {
                 type: 'vector',
                 url:
-                    'http://localhost:8081/data/corpus_urbium_baeticarum.json'
-            });
-            map.current.addLayer({
-                "id": "curvasNivel",
-                "type": "line",
-                "source": "corpus_urbium_baeticarum",
-                "source-layer": "curvasNivel",
-                "paint": { "line-color": "rgba(226, 135, 67, 1)", "line-width": 0.5 }
+                    'http://localhost:8081/data/test.json'
             });
             map.current.addLayer({
                 "id": "casaColumna",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "casaColumna",
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
             });
-
             map.current.addLayer({
                 "id": "casaMartes",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "casaMartes",
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            });  
             map.current.addLayer({
                 "id": "casaNorte",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "casaNorte",
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            }); 
             map.current.addLayer({
                 "id": "casaPeristilo",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "casaPeristilo",
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            });   
             map.current.addLayer({
                 "id": "foro",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "foro",
-                "layout": {
-                    "visibility": "visible"
-                },
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            });   
             map.current.addLayer({
                 "id": "macellum",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "macellum",
-                "layout": {
-                    "visibility": "visible"
-                },
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
-            map.current.addLayer({
-                "id": "termas",
-                "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
-                "source-layer": "termas2019",
-                "layout": {
-                    "visibility": "visible"
-                },
-                "paint": {
-                    "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
-                }
-            });
-            map.current.addLayer({
-                "id": "muralla2Excav_8930N",
-                "type": "fill",
-                "source": "corpus_urbium_baeticarum",
-                "source-layer": "muralla2Excav_8930N",
-                "paint": {
-                    "fill-color": "rgba(181, 163, 163, 1)"
-                }
-            });
-            map.current.addLayer({
-                "id": "muralla1Excav_8930N",
-                "type": "fill",
-                "source": "corpus_urbium_baeticarum",
-                "source-layer": "muralla1Excav_8930N",
-                "paint": {
-                    "fill-color": "rgba(181, 163, 163, 1)"
-                }
-            });
-            map.current.addLayer({
-                "id": "muralla8930N",
-                "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
-                "source-layer": "muralla8930N",
-                "layout": {
-                    "visibility": "visible"
-                },
-                "paint": {
-                    "fill-extrusion-height": 0.5,
-                    "fill-extrusion-color": "#873e23"
-                }
-            });
+            }); 
             map.current.addLayer({
                 "id": "recinto1",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "recinto1",
-                "layout": {
-                    "visibility": "visible"
-                },
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            });  
             map.current.addLayer({
                 "id": "recinto2",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "recinto2",
-                "layout": {
-                    "visibility": "visible"
-                },
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            });  
             map.current.addLayer({
                 "id": "recinto3",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "recinto3",
-                "layout": {
-                    "visibility": "visible"
-                },
                 "paint": {
                     "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
-                    "fill-extrusion-height": 0.5
+                    "fill-extrusion-height": 0.3
                 }
-            });
+            });   
+            map.current.addLayer({
+                "id": "termas",
+                "type": "fill-extrusion",
+                "source": "test",
+                "source-layer": "termas",
+                "paint": {
+                    "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+                    "fill-extrusion-height": 0.3
+                }
+            });   
             map.current.addLayer({
                 "id": "murallaPerimetro8930N",
                 "type": "fill-extrusion",
-                "source": "corpus_urbium_baeticarum",
+                "source": "test",
                 "source-layer": "murallaPerimetro8930N",
-                "layout": {
-                    "visibility": "visible"
-                },
                 "paint": {
-                    "fill-extrusion-height": 0.5,
-                    "fill-extrusion-color": "#873e23"
+                    "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+                    "fill-extrusion-height": 0.3
                 }
-            });
-            map.current.addLayer({
-                "id": "lacus_ligustinus",
-                "type": "fill",
-                "source": "corpus_urbium_baeticarum",
-                "source-layer": "lacus_ligustinus",
-                "paint": {
-                    "fill-color": "rgba(40, 79, 160, 1)",
-                    "fill-opacity": 0.5
-                }
-            });
+            });                                                                                                                
+            // map.current.addSource('corpus_urbium_baeticarum', {
+            //     type: 'vector',
+            //     url:
+            //         'http://localhost:8081/data/corpus_urbium_baeticarum.json'
+            // });
+            // map.current.addLayer({
+            //     "id": "curvasNivel",
+            //     "type": "line",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "curvasNivel",
+            //     "paint": { "line-color": "rgba(226, 135, 67, 1)", "line-width": 0.5 }
+            // });
+            // map.current.addLayer({
+            //     "id": "casaColumna",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "casaColumna",
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+
+            // map.current.addLayer({
+            //     "id": "casaMartes",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "casaMartes",
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "casaNorte",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "casaNorte",
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "casaPeristilo",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "casaPeristilo",
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "foro",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "foro",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "macellum",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "macellum",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "termas",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "termas2019",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "muralla2Excav_8930N",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "muralla2Excav_8930N",
+            //     "paint": {
+            //         "fill-color": "rgba(181, 163, 163, 1)"
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "muralla1Excav_8930N",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "muralla1Excav_8930N",
+            //     "paint": {
+            //         "fill-color": "rgba(181, 163, 163, 1)"
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "muralla8930N",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "muralla8930N",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-height": 0.3,
+            //         "fill-extrusion-color": "#873e23"
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "recinto1",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "recinto1",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "recinto2",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "recinto2",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "recinto3",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "recinto3",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-color": "rgba(154, 150, 150, 0.6)",
+            //         "fill-extrusion-height": 0.3
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "murallaPerimetro8930N",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "murallaPerimetro8930N",
+            //     "layout": {
+            //         "visibility": "visible"
+            //     },
+            //     "paint": {
+            //         "fill-extrusion-height": 0.3,
+            //         "fill-extrusion-color": "#873e23"
+            //     }
+            // });
+            // map.current.addLayer({
+            //     "id": "lacus_ligustinus",
+            //     "type": "fill-extrusion",
+            //     "source": "corpus_urbium_baeticarum",
+            //     "source-layer": "lacus_ligustinus",
+            //     "paint": {
+            //         "fill-color": "rgba(40, 79, 160, 1)",
+            //         "fill-opacity": 0.5
+            //     }
+            // });
         });
         map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
         map.current.addControl(new MapboxStyleSwitcherControl(styles, 'Andalucía OSM OSM Liberty'));
